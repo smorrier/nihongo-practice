a3f76b8befdca2f06de2c684cf8e5f4753f30a77
Sean Morrier <morriersean@gmail.com>
2023-08-21 17:21:53 -0400

    set 6

--- src/context/ActiveSet/vocabSets/index.ts
+++ src/context/ActiveSet/vocabSets/index.ts
@@ -3,6 +3,7 @@ import set2 from './2.json'
 import set3 from './3.json'
 import set4 from './4.json'
 import set5 from './5.json'
+import set6 from './6.json'
 
 export default [
 	set1,
@@ -10,4 +11,5 @@ export default [
 	set3,
 	set4,
 	set5,
+	set6,
 ]
